faee777444b05724cd0fc7187034d4ab8552b933
lpuigo <laurent.puig@gmail.com>
2024-02-16 20:12:30 +0100

    web server version

--- firstprog/main.go
+++ firstprog/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
-	"context"
-	"os"
+	"fmt"
+	"net/http"
+
+	"github.com/a-h/templ"
 )
 
 func main() {
 	component := hello("Laurent")
-	component.Render(context.Background(), os.Stdout)
+	http.Handle("/", templ.Handler(component))
+
+	fmt.Println("Listening on :3000")
+	http.ListenAndServe(":3000", nil)
 }
