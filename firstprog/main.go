61775818e108c44790dfb111032d8410a797682a
lpuigo <laurent.puig@gmail.com>
2024-02-16 22:19:57 +0100

    Syntax and usage (up to attributes)

--- firstprog/main.go
+++ firstprog/main.go
@@ -1,16 +1,40 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-
-	"github.com/a-h/templ"
+	"context"
+	"os"
 )
 
-func main() {
-	component := hello("Laurent")
-	http.Handle("/", templ.Handler(component))
+//func main() {
+//	component := hello("Laurent")
+//	http.Handle("/", templ.Handler(component))
+//
+//	fmt.Println("Listening on :3000")
+//	http.ListenAndServe(":3000", nil)
+//}
 
-	fmt.Println("Listening on :3000")
-	http.ListenAndServe(":3000", nil)
+func main() {
+	ba := booleanAttribute(false)
+	ba.Render(context.Background(), os.Stdout)
+	println()
+
+	ca := conditionalAttribute(true)
+	ca.Render(context.Background(), os.Stdout)
+	println()
+
+	sa := spreadAttributesUsage()
+	sa.Render(context.Background(), os.Stdout)
+	println()
+
+	p1 := Person{
+		URL:  "/github.com/lpuigo",
+		Name: "Laurent",
+	}
+	uc := urlComponent(p1)
+	uc.Render(context.Background(), os.Stdout)
+	println()
+
+	jsB := jsButton("du texte")
+	jsB.Render(context.Background(), os.Stdout)
+	println()
 }
