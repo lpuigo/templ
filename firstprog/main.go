7515eb3ec96687e4cf72a2865a57e424f4b7dab5
lpuigo <laurent.puig@gmail.com>
2024-02-20 21:24:22 +0100

    up to "components"

--- firstprog/main.go
+++ firstprog/main.go
@@ -14,27 +14,41 @@ import (
 //}
 
 func main() {
-	ba := booleanAttribute(false)
-	ba.Render(context.Background(), os.Stdout)
-	println()
+	//ba := booleanAttribute(false)
+	//ba.Render(context.Background(), os.Stdout)
+	//println()
+	//
+	//ca := conditionalAttribute(true)
+	//ca.Render(context.Background(), os.Stdout)
+	//println()
+	//
+	//sa := spreadAttributesUsage()
+	//sa.Render(context.Background(), os.Stdout)
+	//println()
+	//
+	//p1 := Person{
+	//	URL:  "/github.com/lpuigo",
+	//	Name: "Laurent",
+	//}
+	//uc := urlComponent(p1)
+	//uc.Render(context.Background(), os.Stdout)
+	//println()
+	//
+	//jsB := jsButton("du texte")
+	//jsB.Render(context.Background(), os.Stdout)
+	//println()
 
-	ca := conditionalAttribute(true)
-	ca.Render(context.Background(), os.Stdout)
-	println()
+	//dynCssButton("Click me false", false).Render(context.Background(), os.Stdout)
+	//println()
+	//dynCssButton("Click me1 true", true).Render(context.Background(), os.Stdout)
+	//println()
+	//dynCssButton("Click me2 true", true).Render(context.Background(), os.Stdout)
+	//println()
 
-	sa := spreadAttributesUsage()
-	sa.Render(context.Background(), os.Stdout)
+	cssCompButton("Click me2 false", false).Render(context.Background(), os.Stdout)
 	println()
-
-	p1 := Person{
-		URL:  "/github.com/lpuigo",
-		Name: "Laurent",
-	}
-	uc := urlComponent(p1)
-	uc.Render(context.Background(), os.Stdout)
-	println()
-
-	jsB := jsButton("du texte")
-	jsB.Render(context.Background(), os.Stdout)
+	cssCompButton("Click me2 true", true).Render(context.Background(), os.Stdout)
 	println()
 }
+
+// continue with https://templ.guide/core-concepts/components
